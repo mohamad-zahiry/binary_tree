7cdb30a949da2ced3f7d43f38832edf5536858be
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-21 21:42:47 +0430

    Writing test

---
+++ test.py
@@ -0,0 +1,18 @@
+from binary_tree import BinaryTree, BT_to_list
+
+
+def main():
+    from random import randint, seed
+
+    seed(123)
+
+    integers = BinaryTree(randint(0, 100))
+    for x in range(20):
+        integers.child(randint(0, 100))
+
+    sorted_list = BT_to_list(integers)
+    print(sorted_list)
+
+
+if __name__ == "__main__":
+    main()
